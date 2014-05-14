34d926a2df2263ceabb7d658bd21dfc79186e145
Jerome Zago <jerome.zago@gmail.com>
2014-05-14 23:31:20 +0200

    Fairly successful try
    (mount.c:68): Messages from underlying mount program:
    (mount.c:72): NOTE: mount.crypt does not support utab [...]
    
    However it takes several seconds to log in and there is no umount on
    logout.

--- setup.sh
+++ setup.sh
@@ -16,20 +16,24 @@ if ! fgrep -q pam_mount /etc/pam.d/system-auth; then
     patch /etc/pam.d/system-auth /vagrant/system-auth.patch
 fi
 
-### Set up encrypted partition
-
-if ! cryptsetup isLuks $HOME_DEVICE; then
-    echo "$PASSPHRASE" | cryptsetup luksFormat $HOME_DEVICE -
-    cryptsetup --key-file <(echo "$PASSPHRASE") luksOpen $HOME_DEVICE home
-    mkfs.ext4 -q /dev/mapper/home
-    cryptsetup luksClose home || true
-fi
-
 ### Create user account
 
 if ! id $USER_ACCOUNT >/dev/null 2>&1; then
     useradd -M $USER_ACCOUNT
     echo -e "$PASSPHRASE\n$PASSPHRASE\n" | passwd $USER_ACCOUNT
+fi
+
+### Set up encrypted partition
+
+if ! cryptsetup isLuks $HOME_DEVICE; then
+    cryptsetup --key-file <(echo -n "$PASSPHRASE") luksFormat $HOME_DEVICE
+    cryptsetup --key-file <(echo -n "$PASSPHRASE") luksOpen $HOME_DEVICE home
+    mkfs.ext4 -q /dev/mapper/home
+    mount /dev/mapper/home /mnt
+    chown $USER_ACCOUNT.$USER_ACCOUNT /mnt
+    chmod 700 /mnt
+    umount /mnt
+    cryptsetup luksClose home || true
 fi
 
 ### Set up pam_mount
